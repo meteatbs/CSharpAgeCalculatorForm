250bce003a5ac213060684a1713616582b5b29ee
meteatbs <43855185+meteatbs@users.noreply.github.com>
2020-02-22 22:46:05 +0300

    Add files via upload

---
+++ AgeCalculator/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace AgeCalculator
+{
+    partial class dtpCurrentDate
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalculate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dtpDOB = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dtpCurrendate = new System.Windows.Forms.DateTimePicker();
+            this.lblAge = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnCalculate
+            // 
+            this.btnCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalculate.Location = new System.Drawing.Point(521, 225);
+            this.btnCalculate.Name = "btnCalculate";
+            this.btnCalculate.Size = new System.Drawing.Size(168, 51);
+            this.btnCalculate.TabIndex = 0;
+            this.btnCalculate.Text = "Calculate";
+            this.btnCalculate.UseVisualStyleBackColor = true;
+            this.btnCalculate.Click += new System.EventHandler(this.btnCalculate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(257, 113);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(171, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Date Of Birth";
+            // 
+            // dtpDOB
+            // 
+            this.dtpDOB.CustomFormat = "dd-MMM-yyyy";
+            this.dtpDOB.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpDOB.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpDOB.Location = new System.Drawing.Point(510, 107);
+            this.dtpDOB.Name = "dtpDOB";
+            this.dtpDOB.Size = new System.Drawing.Size(200, 38);
+            this.dtpDOB.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(258, 171);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(170, 31);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Current Date";
+            // 
+            // dtpCurrendate
+            // 
+            this.dtpCurrendate.CustomFormat = "dd-MMM-yyyy";
+            this.dtpCurrendate.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtpCurrendate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpCurrendate.Location = new System.Drawing.Point(510, 164);
+            this.dtpCurrendate.Name = "dtpCurrendate";
+            this.dtpCurrendate.Size = new System.Drawing.Size(200, 38);
+            this.dtpCurrendate.TabIndex = 4;
+            // 
+            // lblAge
+            // 
+            this.lblAge.AutoSize = true;
+            this.lblAge.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAge.Location = new System.Drawing.Point(258, 235);
+            this.lblAge.Name = "lblAge";
+            this.lblAge.Size = new System.Drawing.Size(156, 31);
+            this.lblAge.TabIndex = 5;
+            this.lblAge.Text = "Your Age: 0";
+            // 
+            // dtpCurrentDate
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblAge);
+            this.Controls.Add(this.dtpCurrendate);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dtpDOB);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCalculate);
+            this.Name = "dtpCurrentDate";
+            this.Text = "Age Calculator";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalculate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DateTimePicker dtpDOB;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dtpCurrendate;
+        private System.Windows.Forms.Label lblAge;
+    }
+}
+
---
+++ AgeCalculator/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AgeCalculator
+{
+    public partial class dtpCurrentDate : Form
+    {
+        public dtpCurrentDate()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if(dtpCurrendate.Value < dtpDOB.Value)
+                {
+                    MessageBox.Show("Current Date must be greater than DOB");
+                }
+                int Age = dtpCurrendate.Value.Year - dtpDOB.Value.Year;
+                //if add DOB+Age < Current Date
+                if (dtpDOB.Value.AddYears(Age) > dtpCurrendate.Value)
+                {
+                    Age--;
+                    lblAge.Text = "Your Age : " + Age.ToString();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message, "Error Message");
+            }
+        }
+    }
+}
